03a63f8d7daa5cc1e9f59ba088cad1e13b39725b
anlex1 <angel.11jase@gmail.com>
2025-02-13 09:37:15 -0500

    Inicio de proyecto, se crea capa de dominio y se agregan entidades

---
+++ CampeonatosFIFA.Dominio/Entidades/Encuentro.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampeonatosFIFA.Dominio.Entidades
+{
+    [Table("Encuentro")]
+    public class Encuentro
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("IdPais1")]
+        public int IdPais1 { get; set; }
+
+        [Column("IdPais2")]
+        public int IdPais2 { get; set; }
+
+        [Column("IdEstadio")]
+        public int IdEstadio { get; set; }
+
+        [Column("IdFase")]
+        public int IdFase { get; set; }
+
+        [Column("IdCampeonato")]
+        public int IdCampeonato { get; set; }
+
+        [Column("Fecha")]
+        public DateTime Fecha { get; set; }
+
+        [Column("Goles1")]
+        public int Goles1 { get; set; }
+
+        [Column("Goles2")]
+        public int Goles2 { get; set; }
+
+        public Seleccion Pais1 { get; set; }
+        public Seleccion Pais2 { get; set; }
+        public Estadio Estadio { get; set; }
+        public Fase Fase { get; set; }
+        public Campeonato Campeonato { get; set; }
+    }
+}
---
+++ CampeonatosFIFA.Dominio/Entidades/GrupoPais.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampeonatosFIFA.Dominio.Entidades
+{
+    [Table("GrupoPais")]
+    public class GrupoPais
+    {
+        [Column("IdGrupo")]
+        public int IdGrupo { get; set; }
+
+        [Column("IdPais")]
+        public int IdPais { get; set; }
+
+        public Grupo Grupo { get; set; }
+        public Seleccion Pais { get; set; }
+    }
+}
